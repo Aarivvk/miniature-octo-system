a980168e19720610ccc741954d71d6b187716e32
vk <vinaya.rvce@gmail.com>
2020-10-11 20:25:38 +0200

    added support to catkin tools and home path

--- tensor_docker/run.sh
+++ tensor_docker/run.sh
@@ -1,5 +1,5 @@
 IMAGE=tensor/2:local
 
 docker run -u 1000:1000 --gpus=all \
- -e HOME=/project -it --network=host \
-  --rm -v $HOME/dev:/project/dev -w /project $IMAGE bash
+ -it --network=host \
+  --rm -v $HOME/dev:/home/tf/dev -w /home/tf $IMAGE bash
