f8df7ee263b01602ae69aac6c7934a5b6bd84913
vk <vk@pop-os.localdomain>
2020-10-16 12:52:22 +0200

    added the nano and gedit and tf user to tensor

--- tensor_docker/run.sh
+++ tensor_docker/run.sh
@@ -3,4 +3,4 @@ IMAGE=tensor/2:local
 docker run -u 1000:1000 --gpus=all \
  --name vktensor \
  -it --network=host \
-  --rm -v $HOME/dev:/home/tf/dev -w /home/tf $IMAGE bash
+  --rm -v $HOME/dev:/home/tf/dev $IMAGE bash
